d58d525a0a61ffeb5187b80582da9cabd06542c1
luasenvy <luas.envy@gmail.com>
2021-03-20 20:36:15 +0900

    improve code

---
+++ src/services/HelloService.js
@@ -0,0 +1,11 @@
+const {
+  Interfaces: { Service },
+} = require('@luasenvy/rapidfire')
+
+class HelloService extends Service {
+  constructor() {
+    super()
+  }
+}
+
+module.exports = HelloService
