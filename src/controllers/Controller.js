ffdb04ee26ee90614080399af0574246053d1433
luasenvy <luas.envy@gmail.com>
2021-03-24 23:43:45 +0900

    improve code

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,4 +1,4 @@
-const _ = require('lodash')
+const _get = require('lodash/get')
 
 const {
   Interfaces: { Controller: _Controller },
@@ -17,34 +17,71 @@ class Controller extends _Controller {
     return !enums.includes(value)
   }
 
-  async validator({ params = {}, expressions = [] }) {
+  /* ***************************************
+   * Request Validate Function
+   * ************************************* */
+  async validate({ params = {}, expressions = [] }) {
     const validation = {}
 
-    for (const { key, type, enums = [], fn } of expressions) {
-      const value = _.get(params, key)
-      if (type === 'required') {
-        if (this.requiredValue({ value })) return Object.assign(validation, { status: 400, message: `필수 값이 누락되었습니다.: ${key}` })
-      } else if (type === 'enum') {
-        if (this.enumValue({ enums, value }))
-          return Object.assign(validation, {
-            status: 400,
-            message: `"${value}" 값은 "${key}" 값으로 허용되지 않은 값입니다.: ${enums.join(', ')} 중 하나만 허용됩니다.`,
-          })
-      } else if (type === 'fn') {
-        if (fn instanceof Function) {
-          const result = await fn({ value })
-          const { status, message } = result || {}
+    for (const expression of expressions) {
+      const { key, type, fn } = expression
+      const value = _get(params, key)
+      const checker = type === 'fn' ? fn : this[`${type}Check`]
 
-          if (status) return Object.assign(validation, { status, message })
-        } else {
-          throw new Error(`유효성 검사에서 "fn" 형식은 반드시 "Function" 유형이어야 합니다.
-            fn: ${fn}
-          `)
-        }
-      }
+      if (checker instanceof Function) Object.assign(validation, await checker({ ...expression, params, value }))
+      else Object.assign(validation, { status: 501, message: `유효성 검사방식이 지정되지 않았거나 구현되지 않은 방식입니다.: "${type}"` })
+
+      if (validation.status) break
     }
 
     return validation
+  }
+
+  requiredCheck({ key, value }) {
+    if (value == 0 || value) return
+    return { status: 400, message: `필수 값이 누락되었습니다.: ${key}` }
+  }
+
+  numberCheck({ key, value }) {
+    if (value != null && !isNaN(value)) return Number(value)
+    return { status: 400, message: `"${key}" 값은 숫자만 허용됩니다.: ${value}` }
+  }
+
+  enumCheck({ key, value, enums = [] }) {
+    if (enums.includes(value)) return
+
+    return {
+      status: 400,
+      message: `"${value}" 값은 "${key}" 값으로 허용되지 않은 값입니다.: ${enums.join(', ')} 중 하나만 허용됩니다.`,
+    }
+  }
+
+  kebabCaseCheck({ key, value }) {
+    const isValid = /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/.test(value)
+    if (isValid) return
+
+    return { status: 400, message: `"${key}" 값의 형식이 올바르지 않습니다.: ${value}` }
+  }
+
+  lowerCamelCaseCheck({ key, value }) {
+    const isValid = /^[a-z][a-zA-Z0-9]+$/.test(value)
+    if (isValid) return
+
+    return { status: 400, message: `"${key}" 값의 형식이 올바르지 않습니다.: ${value}` }
+  }
+
+  snakeCaseCheck({ key, value }) {
+    const isValid = /^([a-z][a-z0-9]*)(_[a-z0-9]+)*$/.test(value)
+    if (isValid) return
+
+    return { status: 400, message: `"${key}" 값의 형식이 올바르지 않습니다.: ${value}` }
+  }
+
+  UpperCamelCaseCheck({ key, value }) {
+    const isValid = /^[A-Z][a-zA-Z0-9]+$/.test(value)
+    if (isValid) return
+
+    return { status: 400, message: `"${key}" 값의 형식이 올바르지 않습니다.: ${value}` }
   }
 }
 
