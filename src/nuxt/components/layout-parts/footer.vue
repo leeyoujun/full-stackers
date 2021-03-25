407b7b96b38d1075beda2c761e7e7da7c26b4b18
kdg1986 <user@email.mail>
2021-03-25 22:48:06 +0900

    footer layout commit

--- src/nuxt/components/layout-parts/footer.vue
+++ src/nuxt/components/layout-parts/footer.vue
@@ -1,25 +1,21 @@
 <template>
   <footer>
-    <b-container fluid class="bv-example-row">
-      
+    <b-container fluid class="bv-example-row">      
       <b-row class="text-center justify-content-md-center mb-5">
         <b-col col lg="5">content-1</b-col>
         <b-col md="auto">content-2</b-col>
         <b-col col lg="5">content-3</b-col>
-      </b-row>
-      
+      </b-row>      
       <b-row class="text-center justify-content-md-center mb-5">
         <b-col col lg="5">content-4</b-col>
         <b-col md="auto">content-5</b-col>
         <b-col col lg="5">content-6</b-col>
       </b-row>
-
       <b-row class="justify-content-md-center">
         <b-col md="auto">
           <h6 class="mb-0">&copy; {{ year }} Full-Stackers | All Rights Reserved.</h6>
         </b-col>
       </b-row>
-
     </b-container>
   </footer>
 </template>
